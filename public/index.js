35a301c06697f1eb28088bd7ceb8567b9e5bcc41
Emily Yu <bluerainmango@gmail.com>
2020-03-12 03:14:15 -0700

    added carousel, design

--- public/index.js
+++ public/index.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
-  M.AutoInit();
-  $(".carousel").carousel();
+  //   M.AutoInit();
+  $(".carousel").carousel({
+    padding: 10,
+    indicators: true,
+    // noWrap: true,
+    dist: 0,
+    numVisible: 3,
+    shift: 10
+  });
+
+  var instance = M.Carousel.getInstance(
+    document.getElementsByClassName(".carousel")
+  );
+
+  console.log("🍐 ", instance);
+  console.log("🍐 ", instance.pressed);
+  document.getElementsById("d").addEventListener("click", function(e) {
+    console.log("clicked");
+  });
 });
