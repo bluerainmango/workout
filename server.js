84dd31c49a40f35e3496c8d6dca91440d265692d
Emily Yu <bluerainmango@gmail.com>
2020-03-16 20:05:59 -0700

    added heroku remote db uri

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 mongoose
-  .connect(process.env.DB_LOCAL, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
